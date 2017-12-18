1f0a2c94e86d7f7a1bb2fc221aecec6beefa4884
high-u <31367724+high-u@users.noreply.github.com>
2017-12-19 01:23:10 +0900

    create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var nstatic = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new nstatic.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(3000);
